4c38f0ce4aa691ae34fc83fade3ba1cf2e0f160e
Paula Kamijo <93081620+paulakamijo2@users.noreply.github.com>
2021-10-25 10:59:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
 const linksSocialMedia = {
-  github: 'paulakamijo2',
-  youtube: 'paulakamijo',
-  facebook: 'paulakamijo',
-  instagram: 'paulakamijo',
-  twitter: 'paulakamijo'
+  github:'paulakamijo',
+  youtube:'UCD7KYFgwcMu5IZZC2dZiHBQ',
+  facebook:'pkamijo',
+  instagram:'paulakamijo',
+  twitter:'paulakamijo'
 }
 
-function changeSocialMediaLinks() {
-  for (let li of socialLinks.children) {
-    const social = li.getAttribute('class')
-
-    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
-  }
+function changeSocialMediaLinks(){
+    for(let li of socialLinks.children){
+      const social = li.getAttribute('class')
+      li.children[0].href = `https://${social}.com/${
+        linksSocialMedia[social]
+      }`
+      /**alert(li.children[0].href)*/
+    }
 }
-
 changeSocialMediaLinks()
-
+/** ${} chama a variável*/
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      userName.textContent = data.name
-      userBio.textContent = data.bio
-      userLink.href = data.html_url
-      UserImage.src = data.avatar_url
-      userLogin.textContent = data.login
-    })
+  .then(response => response.json())
+  .then (data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImage.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+
 }
 
 getGitHubProfileInfos()
+
+/**ARROW FUNCTIONS
+ * é uma forma contraída da função
+ * function nomeDaFuncao(argumentos){Code}
+ * Damos o nome de uma função anônima, pegamos o argumento
+ * 
+ * argumento=>{ }
+ * (argumento, argumentos=>){}
+ * ()=> {}    sem argumentos
+ * 
+ */
